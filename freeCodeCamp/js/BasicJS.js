7b65bfd78020606d526520d934b7eccf57447ef6
Roflles <igxfrunza@gmail.com>
2023-04-25 23:56:06 +0300

    basicJs 25.04

---
+++ freeCodeCamp/js/BasicJS.js
@@ -0,0 +1,44 @@
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+// -----------------------
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+// -----------------------
+let catName = "Oliver";
+let catSound = "Meow!";
+console.log(catName, catSound);
+// -----------------------
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+// -----------------------
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+let myVar = 87;
+// -----------------------
+// Only change code below this line
+myVar++;
+// Only change code below this line
+myVar--;
+// -----------------------
+const ourDecimal = 5.7;
+// Only change code below this line
+let myDecimal = (5.7);
+// -----------------------
