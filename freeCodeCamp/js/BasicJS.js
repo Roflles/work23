3170a29a6cc5c02ad0a0769fe4fed9dc53119f03
Roflles <igxfrunza@gmail.com>
2023-04-26 16:53:18 +0100

    basickJs 26.04

--- freeCodeCamp/js/BasicJS.js
+++ freeCodeCamp/js/BasicJS.js
@@ -42,3 +42,48 @@ const ourDecimal = 5.7;
 // Only change code below this line
 let myDecimal = (5.7);
 // -----------------------
+let a = 3;
+let b = 17;
+let c = 12;
+// Змініть код лише під цим рядком
+a += 12;
+b += 9;
+c += 7;
+a -= 6;
+b -= 15;
+c -= 1;
+a *= 5;
+b *= 3;
+c *= 10;
+a /= 12;
+b /= 4;
+c /= 11;
+// -----------------------
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Змініть цей рядок
+// -----------------------
+const myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
+//------------------------
+var myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine";
+// Змініть цей рядок
+//------------------------
+const myStr3 = "This is the start. " + "This is the end."; // Змініть цей рядок
+//------------------------
+let myStr4 = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+//------------------------
+// Змініть код лише під цим рядком
+const myName = "Ihor";
+const myStr5 = "My name is " + myName + "and I am well!";
+//------------------------
+// Змініть код під цим рядком
+const someAdjective = "easy";
+let myStr6 = "Learning to code is ";
+myStr6 += someAdjective;
+//------------------------
+// Налаштування
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Змініть код лише під цим рядком
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+//------------------------
