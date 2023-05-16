85118d0336833077c706104cbbb59b4af4a984cf
Roflles <igxfrunza@gmail.com>
2023-05-16 17:08:26 +0100

    shop 16.05

--- freeCodeCamp/react/shop/src/App.js
+++ freeCodeCamp/react/shop/src/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import Header from './componets/Header';
+import Image from './componets/image';
+import logo from './img/logo.png'
 
-function App() {
-  return (
-   <div>Shop</div>
-  );
+class App extends React.Component {
+  helpText = "Help text"
+  render() {
+    return (<div className="name">
+      <Header title="Шапка сайта" />
+      <h1>{this.helpText}</h1>
+      <input placeholder={this.helpText} 
+        onClick={this.inputClick} onMouseEnter={this.mouseOver} />
+      <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+      <Image image={logo} />
+    </div>)
+  }
+
+  inputClick() {console.log("Clicked")}
+  mouseOver() {console.log("Mouse Over")}
 }
 
-export default App;
+export default App
