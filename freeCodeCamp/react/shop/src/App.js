258ac5ea8e8767ec61ca29a9865d57c81ec72c3a
Roflles <igxfrunza@gmail.com>
2023-05-21 16:47:50 +0300

    16/0

--- freeCodeCamp/react/shop/src/App.js
+++ freeCodeCamp/react/shop/src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import Header from './componets/Header';
-import Image from './componets/image';
-import logo from './img/logo.png'
+import Users from './componets/Users';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      helpText: "Help text",
-      userData: ""
-    }
-
-    this.inputClick = this.inputClick.bind(this) //register ivent
-  }
-
+  
   componentDidUpdate(prevProp) {
     if (this.state.helpText !== "Help")
     console.log("Some")
@@ -21,23 +11,16 @@ class App extends React.Component {
 
 
   render() {
-    return (<div className="name">
-      <Header title="Шапка сайта" />
-      <h1>{this.state.helpText}</h1>
-      <h2>{this.state.userData}</h2>
-      <input placeholder={this.state.helpText} 
-        onChange={event => this.setState({userData: event.target.value})}
-        onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-      <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
-      <Image image={logo} />
+    return (<div>
+      <Header title="Список користовачів" />
+      <main>
+        <Users />
+      </main>
+      <aside></aside>
     </div>)
   }
 
-  inputClick() {
-    this.setState({helpText: "Changed"})
-    console.log("Clicked")
-  }
-  mouseOver() {console.log("Mouse Over")}
+ 
 }
 
 export default App
---
+++ freeCodeCamp/react/shop/src/componets/Users.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Users extends React.Component {
+  
+    users = [
+        {
+            id: 1,
+            firstname: 'Bob',
+            lastname: 'Marley',
+            bio: 'Loren ipsum dolor sit amet consectetur, adipisicing elit. Repella',
+            age: '40',
+            isHappy: true
+        },
+        {
+            id: 2,
+            firstname: 'John',
+            lastname: 'Doe',
+            bio: 'Loren ipsum dolor sit amet consectetur, adipisicing elit. Repella',
+            age: '22',
+            isHappy: false
+        }
+    ]
+
+render() {
+  if(this.users.length > 0)
+      return (<div>
+        
+        {this.users.map((el) => (<div className='user' key={el.id}>
+            <h3>{el.firstname} {el.lastname}</h3>
+            <p>{el.bio}</p>
+            <b>{el.isHappy ? 'Щясливий :)' : 'Недуже :('}</b>
+          </div>))}
+      </div>)
+  else
+  return (<div className='user'>
+    <h3>Немає користовачів</h3>
+  </div>) 
+
+  }
+}
+
+export default Users
